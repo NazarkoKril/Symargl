3e71d0049fef4e22d3e74371e31ada480fdd58db
Valleriiia <168529644+Valleriiia@users.noreply.github.com>
2025-08-22 16:49:50 +0300

    add materials drawer and some of size table drawer

---
+++ assets/js/components/help-aside.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const helpAsides = document.querySelectorAll(".help-aside");
+    const overlay = document.querySelector(".overlay");
+
+    helpAsides.forEach(item => {
+        const closeBtn = item.querySelector(".close-btn");
+        const backBtn = item.querySelector(".back-btn");
+        const openBtn = document.getElementById(`${item.dataset.target}-btn`);
+
+        openBtn.addEventListener("click", () => {
+            item.classList.add('open');
+
+            if (!overlay.classList.contains('open')) {
+                overlay.classList.add('open');
+            }
+        });
+
+        closeBtn.addEventListener("click", () => {
+            item.classList.remove('open');
+            overlay.classList.remove('open');
+
+            if (backBtn) {
+                item.parentElement.classList.remove('open');
+            }
+        });
+
+        overlay.addEventListener("click", () => {
+            item.classList.remove('open');
+            overlay.classList.remove('open');
+        });
+
+        if (backBtn) {
+            backBtn.addEventListener("click", () => item.classList.remove('open'));
+        }
+    });
+});
